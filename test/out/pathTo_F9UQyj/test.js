dd52ef9ae32f024380ef5bddb96d293ea036cb30
vuGitH <vladimir@uralov.com>
2020-04-22 01:30:15 +0300

    change for in loop for fetchUrlApp headers

--- test/out/pathTo_F9UQyj/test.js
+++ test/out/pathTo_F9UQyj/test.js
@@ -1,25 +1,24 @@
-var o={
+var o = {};
 
-};
-function testFileType(){
+function testFileType() {
 
-var url='https://drive.google.com/open?id=0B9mOESHtO2LXdy1yS3FIbFY4LXM';
-var id=url.split('id=')[1];
-var file=DriveApp.getFileById(id);
-var mimeT=file.getMimeType();
-var blob=file.getBlob();
-var content=blob.getDataAsString();
-var f=DriveApp.createFile('menuPan.js', content,MimeType.PLAIN_TEXT);
-Logger.log(f.getUrl());
-Logger.log(content);
+  var url = 'https://drive.google.com/open?id=0B9mOESHtO2LXdy1yS3FIbFY4LXM';
+  var id = url.split('id=')[1];
+  var file = DriveApp.getFileById(id);
+  var mimeT = file.getMimeType();
+  var blob = file.getBlob();
+  var content = blob.getDataAsString();
+  var f = DriveApp.createFile('menuPan.js', content, MimeType.PLAIN_TEXT);
+  Logger.log(f.getUrl());
+  Logger.log(content);
 
-Logger.log(mimeT);
+  Logger.log(mimeT);
 
 }
-var test={};
-test.url=ScriptApp.getService().getUrl();
-test.url="http://www.grani.ru";
-test.url="http://www.ej.ru";
+var test = {};
+test.url = ScriptApp.getService().getUrl();
+test.url = "http://www.grani.ru";
+test.url = "http://www.ej.ru";
 /**
  * Tests different manner of reading current file
  * a) UrlFetchUp
@@ -27,24 +26,30 @@ test.url="http://www.ej.ru";
  * c) DriveApp.getFilesByType(mimeType)
  * write back into Drive as plain text to watch;
  */
- function t(){
- var folder=DriveApp.getFolderById('0B9mOESHtO2LXcHcyNlp4SGdJakE');   //My Drive/Work/Aquapowder/eMailsMarkUp
- var response=UrlFetchApp.fetch(test.url);
- var txtRep='';
- var headers=response.getAllHeaders();
- var time=new Date();
- Logger.log('Response Headers:');
- txtRep+='response Headers:\n';
- for(var key in headers){
-   txtRep+=key+'  :  '+headers[key]+'\n';
-   Logger.log(key+'  :  '+headers[key]);
- }
- var domain=test.url;
- var response=UrlFetchApp.fetch(domain).getContentText('windows-1251');
-response.replace(/href\=\"\//gi,'href="'+domain+'/').replace(/src\=\"\//gi,'src="'+domain+'/');
- 
- txtRep+='\n'+response+'\n';
- //txtRep+='\n'+response.getContentText('windows-1251')+'\n';
- var rep=folder.createFile("testReport_"+Utilities.formatDate(time,tZone,'ddMMYY_HH:mm:ss'), txtRep, MimeType.PLAIN_TEXT);
- }
- 
+function t() {
+  //My Drive/Work/Aquapowder/eMailsMarkUp
+  var folder = DriveApp.getFolderById('0B9mOESHtO2LXcHcyNlp4SGdJakE');
+  var response = UrlFetchApp.fetch(test.url);
+  var txtRep = '';
+  /* {Object} */
+  var headers = response.getAllHeaders();
+  var hKeys = Object.keys(headers);
+  var time = new Date();
+  Logger.log('Response Headers:');
+  txtRep += 'response Headers:\n';
+  for (var k=0; k< hKeys.length; k++){
+    var key = hKeys[k];
+    txtRep += key + '  :  ' + headers[key] + '\n';
+    Logger.log(key + '  :  ' + headers[key]);
+  }
+  var domain = test.url;
+  response = UrlFetchApp.fetch(domain).getContentText('windows-1251');
+  response.replace(/href\=\"\//gi, 'href="' + domain + '/').
+           replace(/src\=\"\//gi, 'src="' + domain + '/');
+
+  txtRep += '\n' + response + '\n';
+  //txtRep+='\n'+response.getContentText('windows-1251')+'\n';
+  var rep = folder.createFile("testReport_" + 
+                              Utilities.formatDate(time, tZone, 'ddMMYY_HH:mm:ss'),
+                              txtRep, MimeType.PLAIN_TEXT);
+}
